0d1d519ff93b36d068aa920969673d85c169cb7b
Yang <ybh1760@gmail.com>
2021-01-17 22:31:08 +0900

    JPA를 사용하여 데이터베이스 업데이트

--- src/main/java/com/jojuldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojuldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojuldu.book.springboot.domain.posts;
 
+import com.jojuldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,5 +30,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title,String content){
+        this.title = title;
+        this.content = content;
     }
 }
---
+++ src/main/java/com/jojuldu/book/springboot/web/PostApiController.java
@@ -0,0 +1,30 @@
+package com.jojuldu.book.springboot.web;
+
+import com.jojuldu.book.springboot.service.posts.PostService;
+import com.jojuldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojuldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojuldu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostService postsService;
+
+    @PostMapping("api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
